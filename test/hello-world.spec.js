d9b53fbf51b1841abe2ce7ec682330c9703f5d48
Sheva Xu <shevaxu@outlook.com>
2016-05-04 13:13:36 +0800

    Switch to Ava from Mocha

--- test/hello-world.spec.js
+++ test/hello-world.spec.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import HelloWorld from '../src/components/hello-world'
 import { shallow } from 'enzyme'
-import assert from 'assert'
+import test from 'ava'
 
-describe('<HelloWorld />', () => {
-  it('Has one H1', () => {
-    const wrapper = shallow(<HelloWorld />)
-    assert.equal(wrapper.find('h1').length, 1)
-  })
-  it('Contains `Hello World`', () => {
-    const wrapper = shallow(<HelloWorld />)
-    assert.ok(wrapper.contains('Hello World'))
-  })
+test('<HelloWorld /> has one H1', t => {
+  const wrapper = shallow(<HelloWorld />)
+  t.is(wrapper.find('h1').length, 1)
+})
+
+test('Contains `Hello World`', t => {
+  const wrapper = shallow(<HelloWorld />)
+  t.true(wrapper.contains('Hello World'))
 })
